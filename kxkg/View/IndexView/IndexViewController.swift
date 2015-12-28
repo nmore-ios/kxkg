c98fb2d80cac3cb9ec01df962ba3163db9ed060d
xxpenghao1-ios <xxpenghao1@sina.com>
2015-12-28 14:29:46 +0800

    框架更新

---
+++ kxkg/View/IndexView/IndexViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IndexViewController.swift
+//  kxkg
+//
+//  Created by penghao on 15/12/28.
+//  Copyright © 2015年 penghao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+///首页
+class IndexViewController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
